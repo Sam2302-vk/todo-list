5d485166fea1da6e0c190f741bea4b9d788cf121
Sam2302-vk <samvenkateshkumar@gmail.com>
2024-07-20 16:35:01 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,7 @@
+function todolist(){
+      var adding=document.info.list.value;
+      var add=document.getElementsByClassName("addlist")
+      if(adding==null || adding==""){
+        list.appendchild("addlist");
+      }
+}
