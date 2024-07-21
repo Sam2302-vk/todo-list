435cbca9984dce2c054ba4f2812353c731079cf7
Sam2302-vk <samvenkateshkumar@gmail.com>
2024-07-21 10:22:35 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,7 +1,19 @@
-function todolist(){
-      var adding=document.info.list.value;
-      var add=document.getElementsByClassName("addlist")
-      if(adding==null || adding==""){
-        list.appendchild("addlist");
-      }
+function addTodo() {
+  var todoInput = document.getElementById('todoInput');
+  var todoList = document.getElementById('todoList');
+
+  var li = document.createElement('li');
+  li.textContent = todoInput.value;
+
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'delete';
+  
+  li.appendChild(deleteButton);
+  todoList.appendChild(li);
+
+  todoInput.value = '';
+  deleteButton.addEventListener('click', function() {
+      todoList.removeChild(li);
+  });
 }
